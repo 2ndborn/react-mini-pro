74d5f04715d13caa066a1a8787ba3d23726053a9
Micah Francis <micah.francis69@googlemail.com>
2024-07-24 21:05:20 +0000

    Stateful Greeting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from "./components/FunctionalGreeting";
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+import StatefulGreeting from './components/StatefulGreetingWithPreviousState';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="Nice to meet you!!!" name="Micah" age="34" />
+      <StatefulGreeting greeting="I'm a stateful class component" />
     </div>
   );
 }
