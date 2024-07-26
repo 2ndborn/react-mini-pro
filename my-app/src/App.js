438576fdcc69a317aa76ea6b211ff494d3b6ae05
Micah Francis <micah.francis69@googlemail.com>
2024-07-26 07:20:24 +0000

    conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import FunctionalGreeting from "./components/FunctionalGreeting";
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
 import StatefulGreeting from './components/StatefulGreetingWithPreviousState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I'm a stateful class component" />
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={false} />
     </div>
   );
 }
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,49 @@
+// rfce
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rafce creates the same as above but with arrow function
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// rce creates an export class Component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// rconst creates a constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// clg to log something to the console
+// console.log(object)
+
+// clo to log an object along with its name as a string
+// console.log('first', first)
