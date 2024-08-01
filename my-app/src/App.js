43b50c9d884247a05ddcbe7c9c0ea863cc38c16a
Micah Francis <micah.francis69@googlemail.com>
2024-08-01 20:26:50 +0000

    search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,11 +14,12 @@ import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWUTracker';
 import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
 
 function App() {
   return (
     <div className="App">
-      <ControlledForm />
+      <UncontrolledForm />
     </div>
   );
 }
